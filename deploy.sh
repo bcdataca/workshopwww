60bff607c6734b57cb2a83fd4d17964a6d736030
Aaron Berk (grimpoteuthis) <aberk@math.ubc.ca>
2018-05-06 20:05:08 -0700

    fixed annoying typos

--- deploy.sh
+++ deploy.sh
@@ -16,26 +16,29 @@ DIR=$(pwd)
 
 # find all bcdata-src repositories
 find . -name "workshop20*" -print | while read f; do
-    # put the script file in the correct subfolder
-    cp buildSubsite.sh ./$f/scripts/buildSite.sh
+    #echo "start of loop" &&
+    #pwd | cat
     # go to that directory
     cd "$f" &&
+	#echo "directory:" &&
+	#pwd | cat && 
 	# run the buildSite script to build the site into e.g. public/2017
 	./scripts/buildSite.sh &&
 	# move that site from public/2017 to $DIR/2017
 	find ./public -name "20*" -maxdepth 1 | xargs -I '{}' mv {} $DIR &&
-	# remove the script file
-	rm ./scripts/buildSubsite.sh
+	# go back up a directory
+	cd .. &&
+	pwd | cat
 done
 
 
-read -erp "Push gh-pages to origin? (y/n): " doPushToRemote
+read -erp "Push workshopwebsite to origin? (y/n): " doPushToRemote
 
 if [[ "$doPushToRemote" == "y" ]]
 then
     echo "Pushing to remote..."
     read -erp "Commit Message: " commitMessage
-    cd .. && git add 20* && git commit -m "$commitMessage" && git push origin master
+    git add 20* && git commit -m "$commitMessage" && git push origin master
 fi
 
 
